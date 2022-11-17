03e97ba79f72e8fe67925d777edfa323e942b921
prav3337 <prav3337@CSLH3161.global.chainsys.com>
2022-11-17 12:48:34 +0530

    Constructor

---
+++ Training/src/Constructorr.java
@@ -0,0 +1,15 @@
+
+public class Constructorr {
+
+	int id;
+	String name;
+	long phoneNo;
+	String address;
+	String dept;
+	String experince;
+	
+	 Constructorr (int id, String name){
+		this.id=id;
+		this.name=name;
+	}
+}
