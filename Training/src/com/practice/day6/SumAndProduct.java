0dd595fa0b22d704a2f120df2a3faa875a1e37f4
prav3337 <prav3337@CSLH3161.global.chainsys.com>
2022-11-20 22:56:08 +0530

    Sum and Product of array

--- Training/src/com/practice/day6/SumAndProduct.java
+++ Training/src/com/practice/day6/SumAndProduct.java
@@ -1,9 +1,26 @@
 package com.practice.day6;
 
+import java.util.Scanner;
+
 public class SumAndProduct {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner ps=new Scanner(System.in);
+		System.out.println("Enter the a count of numbers:");
+		int count=ps.nextInt();
+		int arr1[]=new int[count];
+		int sum=0,product=1;
+		System.out.println("Enter the numbers:");
+		for(int i=0;i<count;i++) {
+			arr1[i]=ps.nextInt();
+			sum=sum+arr1[i];
+			product=product*arr1[i];
+			
+		}
+		System.out.println("Sum of Array:"+sum);
+		System.out.println("Sum of Array:"+product);
+		
+		
 
 	}
 
