924cbb176ecfc270b79ef3664dd57ff1e167a8c2
prav3337 <prav3337@CSLH3161.global.chainsys.com>
2022-11-21 00:03:46 +0530

    String count

---
+++ Training/src/com/practice/day6/StringCount.java
@@ -0,0 +1,12 @@
+package com.practice.day6;
+
+public class StringCount {
+
+	public static void main(String[] args) {
+		String sentence="REFRIGERATOR";
+		System.out.println(sentence);
+		System.out.println(sentence.length());
+
+	}
+
+}
